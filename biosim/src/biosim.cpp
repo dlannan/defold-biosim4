d3591afb74693d2ccf0b5414fb13cb553efad0ca
David Lannan <dlannan@internode.on.net>
2022-05-13 17:16:30 +0930

    Node view done for now.

--- biosim/src/biosim.cpp
+++ biosim/src/biosim.cpp
@@ -7,12 +7,16 @@
 #include <dmsdk/sdk.h>
 
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <unordered_map>
 
 // This is included here only for the purpose of unit testing of basic types
 #include "basicTypes.h"
 #include "imageWriter.h"
 #include "simulator.h"
 #include "genome-neurons.h"
+#include "nodesoup.hpp"
 
 #define min(a,b) ((a)<(b)?(a):(b))
 #define max(a,b) ((a)>(b)?(a):(b))
@@ -20,6 +24,79 @@
 namespace BS {
     extern uint8_t makeGeneticColor(const Genome &genome);
 }
+
+nodesoup::adj_list_t LoadNodeData( BS::lineType lines, std::unordered_map<std::string, nodesoup::vertex_id_t> & names )
+{
+    nodesoup::adj_list_t g;
+
+    auto name_to_vertex_id = [&g, &names](std::string name) -> nodesoup::vertex_id_t {
+        if (name[name.size() - 1] == ';') {
+            name.erase(name.end() - 1, name.end());
+        }
+
+        nodesoup::vertex_id_t v_id;
+        auto it = names.find(name);
+        if (it != names.end()) {
+            return (*it).second;
+        }
+
+        v_id = g.size();
+        names.insert({ name, v_id });
+        g.resize(v_id + 1);
+        return v_id;
+    };
+
+    for(int i=0; i<lines.size(); ++i) {
+        std::string line = lines[i];
+        if (line[0] == '}') {
+            break;
+        }
+
+        std::istringstream iss(line);
+        std::string name, edge_sign, adj_name;
+        iss >> name >> edge_sign >> adj_name;
+
+        // add vertex if new
+        nodesoup::vertex_id_t v_id = name_to_vertex_id(name);
+
+        assert(edge_sign == "--" || edge_sign.size() == 0);
+        if (edge_sign != "--") {
+            continue;
+        }
+
+        // add adjacent vertex if new
+
+         nodesoup::vertex_id_t adj_id = name_to_vertex_id(adj_name);
+
+        // add edge if new
+        if (find(g[v_id].begin(), g[v_id].end(), adj_id) == g[v_id].end()) {
+            g[v_id].push_back(adj_id);
+            g[adj_id].push_back(v_id);
+        }
+    }
+    return g;
+}
+
+void GetGenomeColor(const BS::Indiv &indiv, uint8_t * color)
+{
+    constexpr uint8_t maxColorVal = 0xb0;
+    constexpr uint8_t maxLumaVal = 0xb0;
+
+    auto rgbToLuma = [](uint8_t r, uint8_t g, uint8_t b) { return (r+r+r+b+g+g+g+g) / 8; };
+
+    int c = BS::makeGeneticColor(indiv.genome);
+    color[0] = (c);                  // R: 0..255
+    color[1] = ((c & 0x1f) << 3);    // G: 0..255
+    color[2] = ((c & 7)    << 5);    // B: 0..255
+
+    // Prevent color mappings to very bright colors (hard to see):
+    if (rgbToLuma(color[0], color[1], color[2]) > maxLumaVal) {
+        if (color[0] > maxColorVal) color[0] %= maxColorVal;
+        if (color[1] > maxColorVal) color[1] %= maxColorVal;
+        if (color[2] > maxColorVal) color[2] %= maxColorVal;
+    }
+}
+
 
 static int SimulationStart(lua_State* L)
 {
@@ -63,26 +140,11 @@ static int SimulationStep(lua_State* L)
 
     color[0] = color[1] = color[2] = 0x88;
 
-    constexpr uint8_t maxColorVal = 0xb0;
-    constexpr uint8_t maxLumaVal = 0xb0;
-
-    auto rgbToLuma = [](uint8_t r, uint8_t g, uint8_t b) { return (r+r+r+b+g+g+g+g) / 8; };
-    
     for (uint16_t index = 1; index <= BS::p.population; ++index) 
     {
         const BS::Indiv &indiv = BS::peeps[index];
 
-        int c = BS::makeGeneticColor(indiv.genome);
-        color[0] = (c);                  // R: 0..255
-        color[1] = ((c & 0x1f) << 3);    // G: 0..255
-        color[2] = ((c & 7)    << 5);    // B: 0..255
-
-        // Prevent color mappings to very bright colors (hard to see):
-        if (rgbToLuma(color[0], color[1], color[2]) > maxLumaVal) {
-            if (color[0] > maxColorVal) color[0] %= maxColorVal;
-            if (color[1] > maxColorVal) color[1] %= maxColorVal;
-            if (color[2] > maxColorVal) color[2] %= maxColorVal;
-        }
+        GetGenomeColor(indiv, color);
 
         lua_pushnumber(L, indiv.loc.x * BS::p.displayScale);
         lua_rawseti(L, 1, idx++); 
@@ -129,7 +191,12 @@ static int GetAgent(lua_State* L)
     DM_LUA_STACK_CHECK(L,0);
     int coordx = luaL_checknumber(L, 1);
     int coordy = luaL_checknumber(L, 2);
-    luaL_checktype(L, 3, LUA_TTABLE);
+    luaL_checktype(L, 3, LUA_TTABLE);   // Agent data
+    luaL_checktype(L, 4, LUA_TTABLE);   // Points
+    luaL_checktype(L, 5, LUA_TTABLE);   // Names
+    luaL_checktype(L, 6, LUA_TTABLE);   // Joins
+    int icount = luaL_checknumber(L, 7);
+    int k = luaL_checknumber(L, 8);
 
     // Convert coords back to local corrds for indiv
     BS::Coord     loc;
@@ -145,13 +212,64 @@ static int GetAgent(lua_State* L)
         
         BS::Indiv &indiv = BS::peeps.getIndiv( loc );
         BS::lineType   lines;
-        indiv.getIGraphEdgeList(&lines);
 
-        // Should have a bunch of lines of text that are in biosim format
-        BS::lineType::iterator itr = lines.begin();
-        printf("%d   %d    %d\n", loc.x, loc.y, (int)lines.size());
-        for(int i = 0; i < lines.size(); ++i) {
-            printf("NERUAL: %s\n", lines[i].c_str());
+        uint8_t color[3];
+        GetGenomeColor(indiv, color);
+
+        // Store some agent info in the agent table 
+        lua_pushstring(L, "id");
+        lua_pushnumber(L, indiv.index );
+        lua_rawset(L, 5);
+        lua_pushstring(L, "r");
+        lua_pushnumber(L, color[0] );
+        lua_rawset(L, 5);
+        lua_pushstring(L, "g");
+        lua_pushnumber(L, color[1] );
+        lua_rawset(L, 5);
+        lua_pushstring(L, "b");
+        lua_pushnumber(L, color[2] );
+        lua_rawset(L, 5);
+        lua_pushstring(L, "responsiveness");
+        lua_pushnumber(L, indiv.responsiveness );
+        lua_rawset(L, 5);
+
+        // Get all the neural paths
+        indiv.getIGraphEdgeList(&lines);
+        // Build a nodesup compatible data set
+        std::unordered_map<std::string, nodesoup::vertex_id_t> names;
+        nodesoup::adj_list_t list = LoadNodeData( lines, names );
+
+        // List the joins. Simply id <-> id
+        for(int i=0; i<list.size(); ++i) {
+            if(list[i].size() > 0) {
+                for(int j = 0; j<list[i].size(); ++j) {
+                    lua_pushnumber(L, i);
+                    lua_pushnumber(L, list[i][j]);
+                    lua_rawset(L, 6);
+                }
+            }
+        }
+
+        // The list has all the joins of all the nodes. 
+        int width = 512;
+        int height = 512;
+
+        std::vector<nodesoup::Point2D> results = nodesoup::fruchterman_reingold( list, width, height, icount, k );
+        // printf("%d   %d    %d    %d\n", loc.x, loc.y, (int)lines.size(), (int)names.size());
+        int idx = 1;
+        for(int p = 0; p < results.size(); ++p) {
+            lua_pushnumber(L, results[p].x);
+            lua_rawseti(L, 3, idx++);
+            lua_pushnumber(L, results[p].y);
+            lua_rawseti(L, 3, idx++);
+        }
+
+        for (auto& it: names) {
+            // Do stuff
+            // std::cout << it.first << "  " << it.second << std::endl;
+            lua_pushnumber(L, it.second);
+            lua_pushstring(L, it.first.c_str());
+            lua_rawset(L, 4);
         }
     }
     
